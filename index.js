19449810d8cbc747edd4bbf99c815f68716a9928
Raju Pusti <rajupusti@yahoo.com>
2025-06-17 11:07:59 +0000

    vdvd

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
   res.render('index', { });   
