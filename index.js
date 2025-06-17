8cccedbbf4cc7d2604036bd87990ae7194c54170
Raju Pusti <rajupusti@yahoo.com>
2025-06-17 11:04:35 +0000

    hfgf

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { });   
+})
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
