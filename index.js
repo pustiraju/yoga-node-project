1f9a684c1bd60ae85a2e93b22db12f8dda1fa36d
Raju Pusti <rajupusti@yahoo.com>
2025-06-19 13:29:17 +0000

    hfhgs

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
 
-
+// Set view engine
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
+// Serve static files (if using CSS, images, etc.)
+app.use(express.static(__dirname + '/public'));
+
+// Route
 app.get('/', (req, res) => {
-  res.render('index', { });   
-})
+  res.render('index'); 
+});
 
-
-
-
-
-
+// Start server
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "*.ejs",
+    "*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
